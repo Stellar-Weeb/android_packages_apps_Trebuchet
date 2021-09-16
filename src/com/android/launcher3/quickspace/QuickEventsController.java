453fdd08400abd98beaaf0c2d9ac89f30d938939
Nick <nift4@protonmail.com>
2021-09-16 18:57:37 +0200

    fix random crash
    We got less welcome messages

--- src/com/android/launcher3/quickspace/QuickEventsController.java
+++ src/com/android/launcher3/quickspace/QuickEventsController.java
@@ -111,7 +111,7 @@ public class QuickEventsController {
         if (mIsFirstTimeDone) return;
         mIsQuickEvent = true;
         mEventTitle = mContext.getResources().getString(R.string.quick_event_rom_intro_welcome);
-        mEventTitleSub = mContext.getResources().getStringArray(R.array.welcome_message_variants)[getLuckyNumber(0,6)];
+        mEventTitleSub = mContext.getResources().getStringArray(R.array.welcome_message_variants)[getLuckyNumber(0,5)];
         mEventSubIcon = R.drawable.ic_quickspace_crdroid;
 
         mEventTitleSubAction = new OnClickListener() {
